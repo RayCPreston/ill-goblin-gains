b89daa05c1533a82fb535e2278b60d8b4945ca80
Ray.Preston <rpreston@copaair.com>
2026-05-29 14:24:41 -0500

    Good enough camera solution.  Update design doc.  Rebuild PoC level.

--- project/autoloads/map_config.gd
+++ project/autoloads/map_config.gd
@@ -1,15 +1,23 @@
 extends Node
 
-@export var map_width_tiles: int = 72
-@export var map_height_tiles: int = 45
-@export var tile_size: int = 16
+const MIN_TILE_WIDTH: int = 40
+const MAX_TILE_WIDTH: int = 100
+const MIN_TILE_HEIGHT: int = 20
+const MAX_TILE_HEIGHT: int = 60
+
+@export var map_tile_width: int = 72
+@export var map_tile_height: int = 45
 @export var hud_margin_right: int = 128
 
 func get_map_pixel_width() -> int:
-	return map_width_tiles * tile_size
+	return map_tile_width * Constants.TILE_SIZE
 
 func get_map_pixel_height() -> int:
-	return map_height_tiles * tile_size
+	return map_tile_height * Constants.TILE_SIZE
 
 func get_map_aspect_ratio() -> float:
-	return float(map_width_tiles) / float(map_height_tiles)
+	return float(map_tile_width) / float(map_tile_height)
+
+func calculate_map_dimensions(available_pixels: Vector2) -> void:
+	map_tile_width = clampi(int(available_pixels.x / Constants.TILE_SIZE), MIN_TILE_WIDTH, MAX_TILE_WIDTH)
+	map_tile_height = clampi(int(available_pixels.y / Constants.TILE_SIZE), MIN_TILE_HEIGHT, MAX_TILE_HEIGHT)
--- project/scripts/level/camera.gd
+++ project/scripts/level/camera.gd
@@ -4,25 +4,6 @@ func _ready() -> void:
 	call_deferred("fit_camera")
 
 func fit_camera() -> void:
-	var screen_size: Vector2 = Vector2(DisplayServer.screen_get_size())
-	var available_width: float = screen_size.x - MapConfig.hud_margin_right
-	var available_height: float = screen_size.y
-	var scale_x: float = available_width / MapConfig.get_map_pixel_width()
-	var scale_y: float = available_height / MapConfig.get_map_pixel_height()
-	var min_scale: float = minf(scale_x, scale_y)
-	
-	var snapped_scale: float = roundf(min_scale * 4.0) / 4.0
-	
+	var snapped_scale: float = ResolutionManager.get_snapped_scale()
 	zoom = Vector2(snapped_scale, snapped_scale)
-	_set_position(snapped_scale)
-	
-	print("screen_size: ", screen_size)
-	print("raw scale: ", min_scale)
-	print("snapped scale: ", snapped_scale)
-	print("zoom: ", zoom)
-
-func _set_position(snapped_scale: float) -> void:
-	var map_pixel_width: float = MapConfig.get_map_pixel_width()
-	var map_pixel_height: float = MapConfig.get_map_pixel_height()
-	var hud_offset: float = MapConfig.hud_margin_right / 2.0
-	position = Vector2((map_pixel_width / 2.0) - (hud_offset / snapped_scale), map_pixel_height / 2.0)
+	position = Vector2(MapConfig.get_map_pixel_width() / 2.0 - (MapConfig.hud_margin_right / 2.0) / snapped_scale, MapConfig.get_map_pixel_height() / 2.0)
